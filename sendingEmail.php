5db6a131dde1efd26f4b898abed6ace4f36a0c3a
Fábio Henrique Silva Lima <fabiolima@pop-os.localdomain>
2022-01-06 09:07:39 -0300

    readjustment  to layout in command line

--- sendingEmail.php
+++ sendingEmail.php
@@ -10,16 +10,54 @@ sendingEmail();
 
 function sendingEmail()
 {
-    echo "*****************************************************************\n";
-    echo "* Bem vindo(a) ao envia de E-mails de Fábio Henrique Silva Lima *\n"; 
-    echo "*****************************************************************\n";
+    echo "********************************************************************\n";
+    echo "*  Bem vindo(a) ao envia de E-mails de Fábio Henrique Silva Lima   *\n"; 
+    echo "*------------------------------------------------------------------*\n";
     
-    $to = readline("Qual o E-mail do Destinatário:");
-    $name = readline("Qual o Nome do Remetente:");
-    $subject = readline("Qual o Assunto do E-mail:");
-    $text = readline("Qual o Texto do E-mail:");
-    $confirm = readline("Quer mesmo enviar o E-mail {$subject} para {$to} (Y/N):");
+    $to = readline("* Qual o E-mail do Destinatário:");
+    $name = readline("* Qual o Nome do Remetente:");
+    $subject = readline("* Qual o Assunto do E-mail:");
+    $text = readline("* Qual o Texto do E-mail:");
+    $confirm = readline("* Quer mesmo enviar o E-mail {$subject} para {$to} (Y/N):");
+    echo "********************************************************************\n";
+    echo "\n";
 
+    validateData($to,$name,$subject,$text,$confirm);
 }
 
+
+function validateData($to,$name,$subject,$text,$confirm)
+{
+    $errors = [];
+    if(!valid_email($to)) $errors[] = "* E-mail inválido                                                  *\n";
+    if (preg_match("/^([a-zA-Z' ]+)$/",$name) || empty($name)) $errors[] = "* Nome do Remetente está inválido                                  *\n";
+    if (preg_match("/^([a-zA-Z' ]+)$/",$subject) || empty($subject)) $errors[] = "* Assunnto do E-mail está inválido                                 *\n";
+    if (preg_match("/^([a-zA-Z' ]+)$/",$text) || empty($text)) $errors[] = "* Texto do está inválido                                           *\n";
+    if ($confirm == "n" || $confirm = "N") $errors[] = "* Processo cancelado                                               *\n";
+
+    if (!empty($errors)) {
+
+        echo "********************************************************************\n";
+        echo "* Error :( Atenção Repita Todo Processo Novamente com Mais Atenção *\n"; 
+        echo "*------------------------------------------------------------------*\n";
+        foreach($errors as $error) {
+            echo $error;
+        }
+        echo "********************************************************************\n";        
+        echo "\n";
+
+        exit;
+    }
+
+    return true;
+}
+
+function valid_email($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function sendEmail()
+{
+    
+}
 ?>
